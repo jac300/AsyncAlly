d4cb596ca2d395e9e32758a9865bd1da5745f17e
Jennifer Clark <jac300@nyu.edu>
2018-01-06 19:23:25 -0800

    consider void type task

--- AsyncAlly/Classes/AATaskObject.swift
+++ AsyncAlly/Classes/AATaskObject.swift
@@ -5,11 +5,19 @@ public struct AsyncAlly {}
 typealias AATask<ValueType> = (_ success: @escaping (ValueType) -> Void,
     _ failure: @escaping (Error?) -> Void) -> URLSessionDataTask
 
+//typealias AATaskVoid<ValueType> = (_ success: @escaping (ValueType) -> Void,
+//    _ failure: @escaping (Error?) -> Void) -> Void
+
 public struct AATaskObject<ValueType> {
 
     private(set) var task: AATask<ValueType>
+    //private(set) var taskVoid: AATaskVoid<ValueType>?
 
     init(_ task: @escaping AATask<ValueType>) {
         self.task = task
     }
+
+//    init(_ taskVoid: @escaping AATaskVoid<ValueType>) {
+//        self.taskVoid = taskVoid
+//    }
 }
