b9f4ed479241a6795e1f4699b149495f57980af7
Jennifer Clark <jac300@nyu.edu>
2018-01-07 10:20:22 -0800

    combine tests

--- AsyncAlly/AsyncAllyTests/AACombineTests.swift
+++ AsyncAlly/AsyncAllyTests/AACombineTests.swift
@@ -11,20 +11,116 @@ import XCTest
 
 class AsyncAllyCombineTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_combine_1() {
+
+        //Combine [Int], String
+        let expectation = XCTestExpectation(description: "test_combine_1")
+
+        AsyncAlly.combine(MockZips.zip_3_int, AAMockTaskObjects.mockSuccess1, completion: { value, error in
+
+            XCTAssertTrue(error.isEmpty, "0 errors produced.")
+
+            XCTAssertTrue(value.0[0] == 6, "Int with value 6 was stored in the .0[0] position.")
+            XCTAssertTrue(value.0[1] == 7, "Int with value 7 was stored in the .0[1] position.")
+            XCTAssertTrue(value.0[2] == 8, "Int with value 8 was stored in the .0[2] position.")
+
+            XCTAssertTrue(value.1 == "1", "String with value 1 was stored in the .1 position.")
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 100.0)
+    }
+    func test_combine_2() {
+
+        //Combine [Int], [String], String
+        let expectation = XCTestExpectation(description: "test_combine_2")
+
+        AsyncAlly.combine(MockZips.zip_3_int, MockZips.zip_2_str, AAMockTaskObjects.mockSuccess1, completion: { value, error in
+
+            XCTAssertTrue(error.isEmpty, "0 errors produced.")
+
+            XCTAssertTrue(value.0[0] == 6, "Int with value 6 was stored in the .0[0] position.")
+            XCTAssertTrue(value.0[1] == 7, "Int with value 7 was stored in the .0[1] position.")
+            XCTAssertTrue(value.0[2] == 8, "Int with value 8 was stored in the .0[2] position.")
+
+            XCTAssertTrue(value.1[0] == "4", "String with value 4 was stored in the .1[0] position.")
+            XCTAssertTrue(value.1[1] == "5", "String with value 5 was stored in the .1[1] position.")
+
+            XCTAssertTrue(value.2 == "1", "String with value 1 was stored in the .2 position.")
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 100.0)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func test_combine_3() {
+
+        //Combine [Int], Bool, String
+        let expectation = XCTestExpectation(description: "test_combine_3")
+
+        AsyncAlly.combine(MockZips.zip_3_int, AAMockTaskObjects.mockSuccess11, AAMockTaskObjects.mockSuccess2,
+                          completion: { value, error in
+
+            XCTAssertTrue(error.isEmpty, "0 errors produced.")
+
+            XCTAssertTrue(value.0[0] == 6, "Int with value 6 was stored in the .0[0] position.")
+            XCTAssertTrue(value.0[1] == 7, "Int with value 7 was stored in the .0[1] position.")
+            XCTAssertTrue(value.0[2] == 8, "Int with value 8 was stored in the .0[2] position.")
+
+            XCTAssertTrue(value.1 == true, "Bool with value true was stored in the .1 position.")
+
+            XCTAssertTrue(value.2 == "2", "String with value 2 was stored in the .2 position.")
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 100.0)
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_combine_4() {
+
+        //Combine [Int], [String], Bool, String
+        let expectation = XCTestExpectation(description: "test_combine_4")
+
+        AsyncAlly.combine(MockZips.zip_3_int, MockZips.zip_2_str, AAMockTaskObjects.mockSuccess11, AAMockTaskObjects.mockSuccess2,
+                          completion: { value, error in
+
+                            XCTAssertTrue(error.isEmpty, "0 errors produced.")
+
+                            XCTAssertTrue(value.0[0] == 6, "Int with value 6 was stored in the .0[0] position.")
+                            XCTAssertTrue(value.0[1] == 7, "Int with value 7 was stored in the .0[1] position.")
+                            XCTAssertTrue(value.0[2] == 8, "Int with value 8 was stored in the .0[2] position.")
+
+                            XCTAssertTrue(value.1[0] == "4", "String with value 4 was stored in the .1[0] position.")
+                            XCTAssertTrue(value.1[1] == "5", "String with value 5 was stored in the .1[1] position.")
+
+                            XCTAssertTrue(value.2 == true, "Bool with value true was stored in the .2 position.")
+
+                            XCTAssertTrue(value.3 == "2", "String with value 2 was stored in the .3 position.")
+                            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 100.0)
     }
 
+    func test_combine_5() {
+
+        //Merge String, Int, Bool, [String], [Int] with errors
+        let expectation = XCTestExpectation(description: "test_combine_5")
+
+        AsyncAlly.merge(AAMockTaskObjects.mockFailure1, AAMockTaskObjects.mockFailure2,
+                        AAMockTaskObjects.mockFailure3, AAMockTaskObjects.mockFailure4, AAMockTaskObjects.mockFailure5, completion: { value, error in
+
+                            XCTAssertTrue(error.count == 2, "2 errors produced.")
+
+                            XCTAssertNil(value.0, "nil was stored in the .0 position.")
+                            XCTAssertNil(value.1, "nil was stored in the .1 position.")
+                            XCTAssertNil(value.2, "nil was stored in the .2 position.")
+                            XCTAssertNil(value.3, "nil was stored in the .3 position.")
+                            XCTAssertNil(value.4, "nil was stored in the .4 position.")
+                            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 100.0)
+    }
 }
 
--- AsyncAlly/AsyncAllyTests/AAMergeTests.swift
+++ AsyncAlly/AsyncAllyTests/AAMergeTests.swift
@@ -13,7 +13,7 @@ class AsyncAllyMergeTests: XCTestCase {
 
     func test_merge_1() {
 
-        //Zip String, Int
+        //Merge String, Int
         let expectation = XCTestExpectation(description: "test_merge_1")
 
         AsyncAlly.merge(AAMockTaskObjects.mockSuccess1, AAMockTaskObjects.mockSuccess6, completion: { value, error in
@@ -30,7 +30,7 @@ class AsyncAllyMergeTests: XCTestCase {
 
     func test_merge_2() {
 
-        //Zip String, Int, Bool
+        //Merge String, Int, Bool
         let expectation = XCTestExpectation(description: "test_merge_2")
 
         AsyncAlly.merge(AAMockTaskObjects.mockSuccess1, AAMockTaskObjects.mockSuccess6,
@@ -50,18 +50,18 @@ class AsyncAllyMergeTests: XCTestCase {
 
     func test_merge_3() {
 
-        //Zip String, Int, Bool, [String]
+        //Merge String, Int, Bool, Int
         let expectation = XCTestExpectation(description: "test_merge_3")
 
         AsyncAlly.merge(AAMockTaskObjects.mockSuccess1, AAMockTaskObjects.mockSuccess6,
-                        AAMockTaskObjects.mockSuccess9, AAMockTaskObjects.mockSuccess13, completion: { value, error in
+                        AAMockTaskObjects.mockSuccess9, AAMockTaskObjects.mockSuccess7, completion: { value, error in
 
             XCTAssertTrue(error.isEmpty, "0 errors produced.")
 
             XCTAssertTrue(value.0 == "1", "String with value 1 was stored in the .0 position.")
             XCTAssertTrue(value.1 == 6, "Int with value 6 was stored in the .1 position.")
             XCTAssertTrue(value.2 == true, "Bool with value true was stored in the .2 position.")
-            XCTAssertTrue(value.3?[0] == "13", "[String] with value [13] was stored in the .2 position.")
+            XCTAssertTrue(value.3 == 7, "Int with value 7 was stored in the .3 position.")
 
             expectation.fulfill()
         })
@@ -71,40 +71,19 @@ class AsyncAllyMergeTests: XCTestCase {
 
     func test_merge_4() {
 
-        //Zip String, Int, Bool, [String], [Int]
+        //Merge String, Int, Bool, Int
         let expectation = XCTestExpectation(description: "test_merge_4")
 
         AsyncAlly.merge(AAMockTaskObjects.mockSuccess1, AAMockTaskObjects.mockSuccess6,
-                        AAMockTaskObjects.mockSuccess9, AAMockTaskObjects.mockSuccess12, AAMockTaskObjects.mockSuccess16, completion: { value, error in
+                        AAMockTaskObjects.mockSuccess9, AAMockTaskObjects.mockSuccess10, AAMockTaskObjects.mockSuccess7, completion: { value, error in
 
                             XCTAssertTrue(error.isEmpty, "0 errors produced.")
 
                             XCTAssertTrue(value.0 == "1", "String with value 1 was stored in the .0 position.")
                             XCTAssertTrue(value.1 == 6, "Int with value 6 was stored in the .1 position.")
                             XCTAssertTrue(value.2 == true, "Bool with value true was stored in the .2 position.")
-                            XCTAssertTrue(value.3?[0] == "12", "[String] with value [12] was stored in the .3 position.")
-                            XCTAssertTrue(value.4?[0] == 16, "Bool with value true was stored in the .4 position.")
-                            expectation.fulfill()
-        })
-
-        wait(for: [expectation], timeout: 100.0)
-    }
-
-    func test_merge_5() {
-
-        //Zip String, Int, Bool, [String], [Int] with errors
-        let expectation = XCTestExpectation(description: "test_merge_5")
-
-        AsyncAlly.merge(AAMockTaskObjects.mockFailure1, AAMockTaskObjects.mockFailure2,
-                        AAMockTaskObjects.mockFailure3, AAMockTaskObjects.mockFailure4, AAMockTaskObjects.mockFailure5, completion: { value, error in
-
-                            XCTAssertTrue(error.count == 2, "2 errors produced.")
-
-                            XCTAssertNil(value.0, "nil was stored in the .0 position.")
-                            XCTAssertNil(value.1, "nil was stored in the .1 position.")
-                            XCTAssertNil(value.2, "nil was stored in the .2 position.")
-                            XCTAssertNil(value.3, "nil was stored in the .3 position.")
-                            XCTAssertNil(value.4, "nil was stored in the .4 position.")
+                            XCTAssertTrue(value.3 == false, "Bool with value false was stored in the .3 position.")
+                            XCTAssertTrue(value.4 == 7, "Int with value 7 was stored in the .4 position.")
                             expectation.fulfill()
         })
 
@@ -113,11 +92,11 @@ class AsyncAllyMergeTests: XCTestCase {
 
     func test_merge_6() {
 
-        //Zip String, Int, Bool, Int, [String] with errors
+        //Merge String, Int, Bool, Int, String with errors
         let expectation = XCTestExpectation(description: "test_merge_6")
 
         AsyncAlly.merge(AAMockTaskObjects.mockSuccess1, AAMockTaskObjects.mockFailure2,
-                        AAMockTaskObjects.mockFailure3, AAMockTaskObjects.mockSuccess6, AAMockTaskObjects.mockSuccess12, completion: { value, error in
+                        AAMockTaskObjects.mockFailure3, AAMockTaskObjects.mockSuccess6, AAMockTaskObjects.mockSuccess2, completion: { value, error in
 
                             XCTAssertTrue(error.isEmpty, "0 errors produced.")
 
@@ -125,7 +104,26 @@ class AsyncAllyMergeTests: XCTestCase {
                             XCTAssertNil(value.1, "nil was stored in the .1 position.")
                             XCTAssertNil(value.2, "nil was stored in the .2 position.")
                             XCTAssertTrue(value.3 == 6, "Int with value 6 was stored in the .3 position.")
-                            XCTAssertTrue(value.4?[0] == "12", "[String] with value [12] was stored in the .4 position.")
+                            XCTAssertTrue(value.4 == "2", "String with value 2 was stored in the .4 position.")
+                            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 100.0)
+    }
+
+    func test_merge_7() {
+
+        //Merge String, Int, Bool with errors
+        let expectation = XCTestExpectation(description: "test_merge_7")
+
+        AsyncAlly.merge(AAMockTaskObjects.mockFailure1, AAMockTaskObjects.mockFailure2,
+                        AAMockTaskObjects.mockFailure3, completion: { value, error in
+
+                            XCTAssertTrue(error.isEmpty, "0 errors produced.")
+
+                            XCTAssertNil(value.0, "nil was stored in the .0 position.")
+                            XCTAssertNil(value.1, "nil was stored in the .1 position.")
+                            XCTAssertNil(value.2, "nil was stored in the .2 position.")
                             expectation.fulfill()
         })
 
