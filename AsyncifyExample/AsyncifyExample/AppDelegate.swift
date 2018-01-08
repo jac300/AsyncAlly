d6bb3cc5380401d1f53cfdcc5c77e1e092cfe582
Jennifer Clark <jac300@nyu.edu>
2018-01-07 17:18:51 -0800

    completion of example project

--- AsyncifyExample/AsyncifyExample/AppDelegate.swift
+++ AsyncifyExample/AsyncifyExample/AppDelegate.swift
@@ -13,9 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = ExampleViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
@@ -41,6 +42,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
---
+++ AsyncifyExample/AsyncifyExample/Classes/ExViewController+LabelDisplay.swift
@@ -0,0 +1,26 @@
+//
+//  ExampleViewController+LabelDisplay.swift
+//  AsyncifyExample
+//
+//  Created by Jennifer Clark on 1/7/18.
+//  Copyright © 2018 Jennifer Clark. All rights reserved.
+//
+
+import UIKit
+
+extension ExampleViewController {
+
+    func updateProcessingDisplay(_ fNum: Int) {
+        DispatchQueue.main.async { [weak self] in
+            self?.displayLabel.text = (self?.displayLabel.text ?? "") + "\n\nExample Function \(fNum) has completed."
+        }
+    }
+
+    func displayFinalResults(_ strVal1: String?, _ strVal2: String?, _ boolVal: Bool?, _ intVal: Int?, _ errors: [Error]) {
+        let result1Str = "Result for function 1 is \(String(describing:strVal1))."
+        let result2Str = "Result for function 2 is \(String(describing:strVal2))."
+        let result3Str = "Result for function 3 is \(String(describing:boolVal)) because it failed, and the error we received was: \(errors[0])."
+        let result4Str = "Result for function 4 is \(String(describing:intVal))."
+        displayLabel.text = "All functions have now completed: \n\n \(result1Str) \n\n \(result2Str) \n\n \(result3Str) \n\n \(result4Str)"
+    }
+}
